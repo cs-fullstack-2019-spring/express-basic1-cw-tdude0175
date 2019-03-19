26c461e7ac114a1d82cbceaec8e6a3887ec1c40e
tdude0175 <tdude0175@gmail.com>
2019-03-19 16:03:13 -0500

    Added comments to the app file

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ app.use(express.static('public'));
 
 function nameFunction(req,res)
 {
+    // if a get request is sent to the /thomas route it will send back this response
     res.send('Thomas created a server here!');
 }
+// makes a route for the get request.
 app.get('/thomas', nameFunction);
 
+// if a post request is sent to /thomas route it will send back this response instead of the get request
 app.post('/thomas', (req,res) =>
 {
    res.send('Thomas got a post request')
 });
+
+// this assigns the server to the port 8000 where 8000 is substitutable for a different port.
 app.listen(8000,()=>
 {
     console.log('App listening on port 8000');
